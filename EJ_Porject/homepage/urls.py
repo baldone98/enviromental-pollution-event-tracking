7e4d42607fb505a12906358741b985dfee1a73b9
baldone98 <67993113+baldone98@users.noreply.github.com>
2024-03-18 23:23:08 -0500

    fixed issue with report page
    Adjusted urls to be located in same location. Adjusted paths to point to the correct template/.html files.

--- EJ_Porject/homepage/urls.py
+++ EJ_Porject/homepage/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("homepage/", views.index, name="index"),
+    path("report/", views.report_view, name="report"),
 ]
